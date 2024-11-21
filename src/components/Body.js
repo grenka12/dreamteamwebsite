a5ee8d156fb04e89c6def4ffd23c181f7c5695a4
grenka12 <83335334+grenka12@users.noreply.github.com>
2024-11-21 16:26:49 +0200

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,7 @@ import React from "react";
 const people = [
   {
     id: 1,
-    name: "Chornobryvets Yehor",
+    name: "Chornobryvets Yehor2",
     position: "DevOPS Engineer",
     photo: "/yehor_photo.jpg",
   },
